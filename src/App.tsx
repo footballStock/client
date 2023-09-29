cf1649b6d12ee6b623ba96784cdac8a007f51f4e
haejunejung <jhj9422@unist.ac.kr>
2023-09-29 12:56:58 +0900

    initial settings

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Test from './components/Test';
+
+const App = () => {
+  const app = 1;
+
+  return (
+    <>
+      <Test />
+      <h1>React without CRA...</h1>
+    </>
+  );
+};
+
+export default App;
