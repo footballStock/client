8662f03f8a39f77a2ce6816ad357c8665752af55
haejunejung <jhj9422@unist.ac.kr>
2023-10-12 10:47:06 +0900

    recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import {RecoilRoot} from 'recoil';
 import App from './App';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <RecoilRoot>
     <App />
-  </React.StrictMode>,
+  </RecoilRoot>,
 );
