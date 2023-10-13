ed9800a81d007d4dd849dd7f18ae3ce04a3dcb58
haejunejung <jhj9422@unist.ac.kr>
2023-10-13 18:29:16 +0900

    tailwindcss settings

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {RecoilRoot} from 'recoil';
 import App from './App';
+import './styles/tailwind.css';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
