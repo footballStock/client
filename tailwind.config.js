94bfbafceffc532db3926a28f86b1c6a21a7f205
haejunejung <jhj9422@unist.ac.kr>
2023-11-11 11:55:08 +0900

    [#1] feat : Firebase Log in 기능 구현
    - Firebase Email&Password 로그인 기능 구현
    - Firebase Social Login(Google) 로그인 기능 구현
    - UI 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         'custom-green': '#70B8BC',
         'custom-room': '#F3F2F2',
         'custom-chat': '#FDF7F7',
+        'custom-white-gray': '#6D6D6D',
       },
     },
   },
