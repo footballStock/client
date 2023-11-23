db16d99ae1136e1ffe6c0a0bab4a242340355b7b
inu0104 <inu0104@gmail.com>
2023-11-23 18:21:04 +0900

    [#26] feat : page 이동 기능 구현

--- src/Posts/RecentPosts.tsx
+++ src/Posts/RecentPosts.tsx
@@ -9,8 +9,11 @@ import Account_img1 from '../static/others/account_img1.png';
 import Account_img2 from '../static/others/account_img2.png';
 import {getTimeAgo} from '../commons/util';
 
+const POSTS_PER_PAGE = 6;
+
 const RecentPosts: React.FC<{}> = ({}) => {
   const [postdatas, setPostdatas] = useState<Postdata[] | null>();
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태
 
   const getPostList = async () => {
     return getData('/posts/').then(result => {
@@ -38,11 +41,39 @@ const RecentPosts: React.FC<{}> = ({}) => {
     navigate(`/posts/${id}`);
   };
 
+  const currentPosts = postdatas
+    ? postdatas.slice(
+        (currentPage - 1) * POSTS_PER_PAGE,
+        currentPage * POSTS_PER_PAGE
+      )
+    : [];
+
+    const handlePageChange = (pageNumber: number) => {
+      setCurrentPage(pageNumber);
+    };
+  
+    // 페이지 네비게이션 컨트롤을 렌더링하는 함수
+    const renderPageNumbers = () => {
+      const totalPages = Math.ceil((postdatas?.length || 0) / POSTS_PER_PAGE);
+      return Array.from({ length: totalPages }, (_, i) => (
+        <button
+          key={i + 1}
+          onClick={() => handlePageChange(i + 1)}
+          disabled={currentPage === i + 1}
+          className={`mx-1 px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 
+                      ${currentPage === i + 1 ? 'bg-blue-500 text-white' : 'bg-white'}`}
+        >
+          {i + 1}
+        </button>
+      ));
+    };
+
+
   return (
     <div className="justify-center">
       <h1 className="my-5 text-sm font-bold justify-start">Recent</h1>
       <div className="my-3 items-center justify-center grid gap-4 grid-cols-2">
-        {postdatas?.map((postdata, index) => (
+        {currentPosts.map((postdata, index) => (
           <div
             key={index}
             onClick={() => handlePostClick(postdata.id)}
@@ -70,6 +101,9 @@ const RecentPosts: React.FC<{}> = ({}) => {
             />
           </div>
         ))}
+      </div>
+      <div className="flex justify-center mt-4">
+        {renderPageNumbers()}
       </div>
     </div>
   );
