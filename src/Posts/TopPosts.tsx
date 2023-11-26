88db5c58d5b70b7fd69edbd2233e09ebb1e4e56c
inu0104 <inu0104@gmail.com>
2023-11-27 03:07:17 +0900

    [#37] feat : postmain 디자인 완료
    -lime clamp가 잘 안됨

--- src/Posts/TopPosts.tsx
+++ src/Posts/TopPosts.tsx
@@ -2,13 +2,6 @@ import React, {useEffect, useState} from 'react';
 import {Postdata} from 'src/states/types';
 import {useNavigate} from 'react-router-dom';
 import {getData} from '../commons/api';
-
-import Post_image_test1 from '../static/others/Post_image_test1.png';
-import Post_image_test2 from '../static/others/Post_image_test2.png';
-import Post_image_test3 from '../static/others/Post_image_test3.png';
-import Account_img1 from '../static/others/account_img1.png';
-import Account_img2 from '../static/others/account_img2.png';
-import Account_img3 from '../static/others/account_img3.png';
 import {getTimeAgo} from '../commons/util';
 import CreatePost from './CreatePost';
 
@@ -43,7 +36,7 @@ const TopPosts: React.FC<{}> = ({}) => {
     <div className="justify-center">
       
       <div id="top3-posts">
-        <h1 className="text-xl font-bold">Top3 Posts</h1>
+        <h1 id="top3-post-name">Top3 Posts</h1>
         <CreatePost />
       </div>
       
